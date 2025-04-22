43d0d5c7f6a212103c2482a7fdb699892b916b9d
diples77 <151831670+diples77@users.noreply.github.com>
2025-04-22 23:37:06 +0300

    Delete keyboards directory

--- keyboards/admin.py
+++
@@ -1,9 +0,0 @@
-from aiogram.utils.keyboard import InlineKeyboardBuilder
-
-async def admin_command():
-    ikb = InlineKeyboardBuilder()
-
-    ikb.button(text="Рассылка📩", callback_data="mailing")
-    ikb.adjust(1, 2)
-    return ikb.as_markup()
-
